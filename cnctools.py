33bf26cb1022979906e194a79c8924c11e354f97
lab-pc8 <cn@das-lab.net>
2016-11-30 21:23:53 +0100

    cnctools added

---
+++ cnctools.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+from subprocess import call
+
+CWD = os.getcwd()
+
+def setup(args):
+	if(len(args) > 0):
+		if(args[0] == 'msl'):
+			if(prompt("Do you really want to start the MSL setup? Wie oft?")):
+				call(['sudo', './setup/msl-setup.sh'])
+
+		elif(args[0] == 'ttb'):
+			if(prompt("Do you really want to start the MSL setup?")):
+				call(['sudo', './setup/ttb-setup.sh'])
+	else:
+		print("Choose msl, ttb or msl-robot")
+
+
+def prompt(text, default=False):
+	answer = str.lower(input(text + " [" + ("Y" if default else "y") + "/" + ("n" if default else "N") + "] "))
+
+	if(answer == 'y'):
+		return True
+	
+	if(answer == 'n'):
+		return False
+
+	if(answer == ''):
+		return default
+
+	# wrong input, repeat
+	return prompt(text, default)
+
+
+tools = {
+	'setup': setup
+}
+
+
+# Main Script
+
+parser = argparse.ArgumentParser(description="Tools that help us getting boring stuff done.")
+parser.add_argument('tool', choices=tools.keys(), type=str, help="the name of the tool")
+parser.add_argument('args', nargs=argparse.REMAINDER, type=str, help="args for the tool, see <tool> -h")
+
+args = parser.parse_args()
+print("Tool:", args.tool)
+print("Args:", args.args)
+
+# run tool
+if args.tool in tools:
+	tools[args.tool](args.args)
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+chmod ls+x cnctools.py
+sudo ln -s ${PWD}/cnctools.py /usr/local/bin/cnctools
